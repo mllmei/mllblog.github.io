ba124cc2f5b9c026a7cb1788debff3b901215632
mllmei <mllmei@bu.edu>
2020-05-11 17:45:19 -0700

    Updates

---
+++ precache-manifest.80396fcd6fdf37e8ab78ed1898bbb584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee3efcfa77e8ce7fefc996c7c0cff01",
+    "url": "/mllblog/index.html"
+  },
+  {
+    "revision": "ba56f9eace4a2932a41d",
+    "url": "/mllblog/static/js/2.28e5cb51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mllblog/static/js/2.28e5cb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90d0848efd1c6e41d39f",
+    "url": "/mllblog/static/js/main.2be2107b.chunk.js"
+  },
+  {
+    "revision": "4d52007f152ac168d5b2",
+    "url": "/mllblog/static/js/runtime-main.6ff23483.js"
+  },
+  {
+    "revision": "e8a04f6d2ca27db8c46c6f2fe4fb2eb7",
+    "url": "/mllblog/static/media/myAvatar.e8a04f6d.png"
+  }
+]);
